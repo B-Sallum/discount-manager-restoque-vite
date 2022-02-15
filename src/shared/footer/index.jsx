b65ed034746af8d6bedf70c108a17afd6afd7444
Brayan <89050408+brabrahen@users.noreply.github.com>
2022-02-15 19:17:26 -0300

    ajustes finais sobre (#1)

--- src/shared/footer/index.jsx
+++ src/shared/footer/index.jsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './styles.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles.css";
 
 const Footer = () => {
-
   return (
     <div className="footer">
       <h5>
-          Site desenvolvido pela 
-
-        {/* <Link to={"/about"} className="link-golden"> */}
-          &nbsp;Equipe 06  
-        {/* </Link> */}
-          &nbsp;- Classe 007 @&nbsp;
-        <a href="https://blueedtech.com.br/quem-somos/"
-        target="_blank" rel="noopener noreferrer"
-        className="link-blue"
+        Site desenvolvido pela
+        <Link to={"/about"} className="link-golden">
+          &nbsp;Equipe 06
+        </Link>
+        &nbsp;- Classe 007 @&nbsp;
+        <a
+          href="https://blueedtech.com.br/quem-somos/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-blue"
         >
-          Blue Edtech 
+          Blue Edtech
         </a>
-          &nbsp;para&nbsp;
-        <a href="https://www.restoque.com.br/#quem-somos"
-        target="_blank" rel="noopener noreferrer"
-        className="link-golden"
+        &nbsp;para&nbsp;
+        <a
+          href="https://www.restoque.com.br/#quem-somos"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-golden"
         >
           Restoque&reg;
         </a>
-          &nbsp;Todos os direitos reservados
+        &nbsp;Todos os direitos reservados
       </h5>
     </div>
   );
