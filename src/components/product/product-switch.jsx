b6b934855521630cbea9260eafb132f15c558a93
B-Sallum <brusallum@hotmail.com>
2022-02-16 13:38:03 -0300

    Product Switch ok

--- src/components/product/product-switch.jsx
+++ src/components/product/product-switch.jsx
@@ -3,13 +3,16 @@ import { useSwitchContext } from "../../contexts/switch-product-context";
 import api from "../../auth/api";
 
 import { AiFillCloseCircle } from 'react-icons/ai';
+import { useProductsContext } from "../../contexts/products-list";
 
 const ProductSwitch = () => {
 
   const { switchProduct, setSwitchProduct } = useSwitchContext();
+  const { loadProducts } = useProductsContext();
 
   const [productCode, setProductCode] = useState('');
   const [productName, setProductName] = useState('');
+  const [productActive, setProductActive] = useState('');
 
   let product = '';
 
@@ -18,14 +21,54 @@ const ProductSwitch = () => {
     product = getProduct.data;
     setProductCode(product.code);
     setProductName(product.name);
+    if (product.active) {
+      setProductActive('ATIVO');
+    } else {
+      setProductActive('DESATIVADO');
+    }
   }, []);
 
-  return (
-    <div className='modal-bg'>
-      <div className='wrap-modal'>
-        {productCode}
-        {productName}
+  const disableProduct = () => {
+    api.patch(`/products/${productCode}`, { active: false })
+      .then(() => {
+        setSwitchProduct(null);
+        loadProducts();
+      });
+  };
 
+  const enableProduct = () => {
+    api.patch(`/products/${productCode}`, { active: true })
+      .then(() => {
+        setSwitchProduct(null);
+        loadProducts();
+      });
+  };
+
+  return (
+    <div className="modal-bg">
+      <div className="wrap-modal card-add flex-ctr">
+        <div className="flex-ctr col">
+          <div>
+            O produto
+          </div>
+          <div>
+            {productCode} - {productName}
+          </div>
+          <div>
+            Está {productActive} no momento
+          </div>
+          <div>
+            O que deseja fazer?
+          </div>
+          <div className="flex-ctr">
+            <button onClick={enableProduct}>
+              Ativar
+            </button>
+            <button onClick={disableProduct}>
+              Desativar
+            </button>
+          </div>
+        </div>
         <AiFillCloseCircle
           className="close-modal"
           onClick={() => {
